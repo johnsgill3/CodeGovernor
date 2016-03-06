83969339274cc8d9c501040dcdd1dfe7d92dbe28
John Gill <king2bgill@gmail.com>
2016-03-05 22:10:04 -0800

    Update session control to use new User Model

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,20 @@
 class SessionsController < ApplicationController
-  def create
-    render text: request.env['omniauth.auth'].to_json
-  end
+    def create
+        begin
+            @user = User.from_omniauth(request.env['omniauth.auth'])
+            session[:user_id] = @user.id
+            redirect_to user_path(@user)
+        rescue
+            flash[:warning] = "There was an error while trying to authenticate you..."
+            redirect_to root_path
+        end
+
+    end
+
+    def destroy
+        if current_user
+            session.delete(:user_id)
+        end
+        redirect_to root_path
+    end
 end
