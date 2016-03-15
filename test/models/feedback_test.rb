142e235c59a4e489b5a6151d3dfa8b852a850a0c
John Gill <king2bgill@gmail.com>
2016-03-14 23:03:32 -0700

    Write basic association tests for Feedback

--- test/models/feedback_test.rb
+++ test/models/feedback_test.rb
@@ -1,7 +1,27 @@
 require 'test_helper'
 
 class FeedbackTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+    test 'belongs to review' do
+        1500.times do |f|
+            feedback = feedbacks("feedback_#{f}".to_sym)
+            assert_not_nil(feedback, "Could not find feedback #{f}")
+            assert_not_nil(feedback.review, "Could not find review for feedback #{feedback.id}")
+        end
+    end
+
+    test 'belongs to user' do
+        1500.times do |f|
+            feedback = feedbacks("feedback_#{f}".to_sym)
+            assert_not_nil(feedback, "Could not find feedback #{f}")
+            assert_not_nil(feedback.user, "Could not find user for feedback #{feedback.id}")
+        end
+    end
+
+    test 'state is valid' do
+        1500.times do |f|
+            feedback = feedbacks("feedback_#{f}".to_sym)
+            assert_not_nil(feedback, "Could not find feedback #{f}")
+            assert(feedback.state.to_i >= 0 && feedback.state.to_i <= 2, "Invalid state for feedback #{feedback.id}")
+        end
+    end
 end
