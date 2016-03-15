6897be9f026ad6f6a679e8744dc262b097e73d6a
John Gill <king2bgill@gmail.com>
2016-03-14 21:07:10 -0700

    Write basic Review tests

--- test/models/review_test.rb
+++ test/models/review_test.rb
@@ -2,8 +2,34 @@ require 'test_helper'
 
 class ReviewTest < ActiveSupport::TestCase
     test 'belongs to repository' do
-        200.times do |r|
-            puts "Review #{r} belongs to repository #{Review.find_by(pr: r).repository.ghid}"
+        300.times do |r|
+            review = Review.find_by(pr: r)
+            assert_not_nil(review, "Could not find review #{r}")
+            assert_not_nil(review.repository, "Could not find repository for review #{review.pr}")
+        end
+    end
+
+    test 'has files' do
+        300.times do |r|
+            review = Review.find_by(pr: r)
+            assert_not_nil(review, "Could not find review #{r}")
+            assert_not_empty(review.g_files, "Could not find files for review #{review.pr}")
+        end
+    end
+
+    test 'has feedback' do
+        300.times do |r|
+            review = Review.find_by(pr: r)
+            assert_not_nil(review, "Could not find review #{r}")
+            assert_not_empty(review.feedbacks, "Could not find any feedback for review #{review.pr}")
+        end
+    end
+
+    test 'state is valid' do
+        300.times do |r|
+            review = Review.find_by(pr: r)
+            assert_not_nil(review, "Could not find review #{r}")
+            assert(review.state.to_i >= 0 && review.state.to_i <= 2, "Invalid state for review #{review.pr}")
         end
     end
 end
