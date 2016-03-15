c328141c03e8f7a28bec7ab8d8aedf213c41c6b7
John Gill <king2bgill@gmail.com>
2016-03-14 21:06:28 -0700

    Write basic GFile tests

--- test/models/g_file_test.rb
+++ test/models/g_file_test.rb
@@ -1,7 +1,27 @@
 require 'test_helper'
 
 class GFileTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+    test 'has repository' do
+        1500.times do |f|
+            file = g_files("file_#{f}".to_sym)
+            assert_not_nil(file, "Could not find file #{f}")
+            assert_not_nil(file.repository, "Could not find repository for file #{file.name}")
+        end
+    end
+
+    test 'has user' do
+        1500.times do |f|
+            file = g_files("file_#{f}".to_sym)
+            assert_not_nil(file, "Could not find file #{f}")
+            assert_not_nil(file.user, "Could not find user for file #{file.name}")
+        end
+    end
+
+    test 'has reviews' do
+        1500.times do |f|
+            file = g_files("file_#{f}".to_sym)
+            assert_not_nil(file, "Could not find file #{f}")
+            assert_not_empty(file.reviews, "Could not find any reviews for file #{file.name}")
+        end
+    end
 end
