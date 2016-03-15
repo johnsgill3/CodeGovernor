f5d0fe7f372b6eb95ff8459d0eb67c9c0eece143
John Gill <king2bgill@gmail.com>
2016-03-14 21:06:50 -0700

    Write basic Repository tests

--- test/models/repository_test.rb
+++ test/models/repository_test.rb
@@ -2,33 +2,42 @@ require 'test_helper'
 
 class RepositoryTest < ActiveSupport::TestCase
     test 'has users' do
-        100.times do |r|
-            puts "Repository #{r} has user(s):"
-            Repository.find_by(ghid: r).users.each do |u|
-                puts "    #{u.nickname}"
-            end
+        10.times do |r|
+            repo = Repository.find_by(ghid: r)
+            assert_not_nil(repo, "Could not find repo #{r}")
+            assert_not_empty(repo.users, "Could not find any users for repo #{repo.ghid}")
         end
     end
 
     test 'has files' do
-        100.times do |r|
-            puts "Repository #{r} has file(s):"
-            Repository.find_by(ghid: r).g_files.each do |f|
-                puts "    #{f.name}"
-            end
+        10.times do |r|
+            repo = Repository.find_by(ghid: r)
+            assert_not_nil(repo, "Could not find repo #{r}")
+            assert_not_empty(repo.g_files, "Could not find any files for repo #{repo.ghid}")
         end
     end
 
     test 'has reviews' do
-        100.times do |r|
-            puts "Repository #{r} has review(s):"
-            Repository.find_by(ghid: r).reviews.each do |r|
-                puts "    #{r.pr} #{r.state}"
-            end
+        10.times do |r|
+            repo = Repository.find_by(ghid: r)
+            assert_not_nil(repo, "Could not find repo #{r}")
+            assert_not_empty(repo.reviews, "Could not find any reviews for repo #{repo.ghid}")
         end
     end
 
-    test 'enabled' do
+    test 'enabled not null' do
+        10.times do |r|
+            repo = Repository.find_by(ghid: r)
+            assert_not_nil(repo, "Could not find repo #{r}")
+            assert_not_nil(repo.enabled, "Repo #{repo.ghid} has null enabled")
+        end
+    end
 
+    test 'secret_key not null' do
+        10.times do |r|
+            repo = Repository.find_by(ghid: r)
+            assert_not_nil(repo, "Could not find repo #{r}")
+            assert_not_nil(repo.secret_key, "Repo #{repo.ghid} has null secret_key")
+        end
     end
 end
