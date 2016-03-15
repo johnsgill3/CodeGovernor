9bb14fb86db3fb5ee91bdf701646ba88239896f5
John Gill <king2bgill@gmail.com>
2016-03-14 23:02:41 -0700

    Change review association to be through GFiles

--- app/models/repository.rb
+++ app/models/repository.rb
@@ -1,7 +1,7 @@
 class Repository < ActiveRecord::Base
     has_many :g_files, dependent: :destroy
     has_and_belongs_to_many :users
-    has_many :reviews, dependent: :destroy
+    has_many :reviews, through: :g_files
 =begin
     ghid:integer:index
     enabled:boolean
