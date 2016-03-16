5233fd09c7bee19aad114686198e17f49455af58
John Gill <king2bgill@gmail.com>
2016-03-16 10:19:04 -0700

    Modify log format to use UTC and iso8601 format

--- lib/custom_formatter.rb
+++ lib/custom_formatter.rb
@@ -1,15 +1,5 @@
 class CustomFormatter < Logger::Formatter
     def call(severity, datetime, prognm, message)
-        "[#{datetime.strftime("%Y-%m-%d %H:%M:%S")}] #{severity.ljust(5)}: #{message}\n"
+        "[#{datetime.utc.iso8601}] #{severity.ljust(5)}: #{message}\n"
     end
-
-=begin
-    Timestamp Format values
-        %Y - Year with century
-        %m - Month of the year, zero-padded
-        %d - Day of the month, zero-padded
-        %H - Hour of the day, 24-hour clock, zero-padded
-        %M - Minute of the hour
-        %S - Second of the minute
-=end
 end
