985582a409f049f48dd499ceb50e92270ee28658
John Gill <king2bgill@gmail.com>
2016-03-16 18:55:47 -0700

    Setup Webhook Controller for testing
        - Setup event handler skeleton in handler
        - Change code to use Repository.secret_key instead of env
        - Handle payload for both github and test env

--- app/controllers/webhook_controller.rb
+++ app/controllers/webhook_controller.rb
@@ -2,29 +2,42 @@ class WebhookController < ApplicationController
     skip_before_action :verify_authenticity_token
 
     def handle_payload
+        # The following is a 'band-aid' to make testing and production work. The
+        # requests from GitHub come in as a string, but requests from tests come
+        # in as a ActionController::Parameters hash. So only call JSON.parse the
+        # parameters if they are a string.
         @payload = params[:payload]
+        @payload = JSON.parse(params[:payload]) if @payload.is_a?(String)
         @payload_body = request.body.read
 
         # Make sure that this hook came from a known repo
-        verify_signature(@payload_body)
+        repo = Repository.find_by(ghid: @payload["repository"]["id"])
+        return render(plaib: "Repository not found", status: :not_found) if repo == nil
+        WebhookHelper.verify_signature(repo.secret_key, @payload_body, request.env['HTTP_X_HUB_SIGNATURE'])
 
         case request.env['HTTP_X_GITHUB_EVENT']
             when "pull_request"
                 if @payload["action"] == "opened"
                     logger.debug "WHC Received: pull_request[opened]"
+                    WebhookHelper.pr_opened(@payload)
                 elsif @payload["action"] == "synchronized"
                     logger.debug "WHC Received: pull_request[synchronized]"
+                    WebhookHelper.pr_synchronized(@payload)
                 elsif @payload["action"] == "closed"
                     logger.debug "WHC Received: pull_request[closed]"
+                    WebhookHelper.pr_closed(@payload)
                 elsif @payload["action"] == "reopened"
                     logger.debug "WHC Received: pull_request[reopened]"
+                    WebhookHelper.pr_reopened(@payload)
                 else
                     logger.debug "WHC Received: pull_request[Unknown action]"
                 end
             when "issue_comment"
                 logger.debug "WHC Received: issue_comment"
+                WebhookHelper.issue_comment(@payload)
             when "ping"
                 logger.debug "WHC Received: ping"
+                WebhookHelper.ping(@payload)
                 # Send initial status message so Protected Branch can be setup
             else
                 logger.debug "WHC Received: Unknown Event"
@@ -33,11 +46,5 @@ class WebhookController < ApplicationController
         # For now don't send anything in body of message.
         # In future might add status message here
         render plain: "Received #{request.env['HTTP_X_GITHUB_EVENT']} Event"
-    end
-
-    def verify_signature(payload_body)
-        # TODO: Create Secret Key for each repo. Store in DB
-        signature = 'sha1=' + OpenSSL::HMAC.hexdigest(OpenSSL::Digest.new('sha1'), ENV['WEBHOOK_SECRET_KEY'], payload_body)
-        return halt 500, "Signatures didn't match!" unless Rack::Utils.secure_compare(signature, request.env['HTTP_X_HUB_SIGNATURE'])
     end
 end
