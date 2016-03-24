4563f67ddf12812a425ab36ab9db0d3c384afb95
John Gill <king2bgill@gmail.com>
2016-03-23 22:42:55 -0700

    Move render call to hook controller from helper

--- app/helpers/webhook_helper.rb
+++ app/helpers/webhook_helper.rb
@@ -1,8 +1,7 @@
 module WebhookHelper
     def self.verify_signature(key, payload_body, gh_signature)
         signature = 'sha1=' + OpenSSL::HMAC.hexdigest(OpenSSL::Digest.new('sha1'), key, payload_body)
-        return render(status: :bad_request, plain: "Signatures didn't match!") \
-            unless Rack::Utils.secure_compare(signature, gh_signature)
+        Rack::Utils.secure_compare(signature, gh_signature)
     end
 
     def self.pr_opened(_payload)
